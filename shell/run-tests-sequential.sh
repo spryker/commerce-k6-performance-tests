4ade2e8f1f193ef70de37ba0ef4e205376c46a02
Florian Kraemer <florian.kraemer@spryker.com>
2023-09-20 16:32:56 +0200

    Refining reporting and adding S3 upload script.

--- shell/run-tests-sequential.sh
+++ shell/run-tests-sequential.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 # Generate a timestamp
-timestamp=$(date +%Y%m%d%H%M%S)
+timestamp=$(date +%Y%m%d_%H%M%S)
+reportFile='k6_result_'$timestamp'.json';
+outputFolder='results/'$(date +%Y/%m/%d);
+outputFolder2='../../../'$outputFolder;
+
+# Check if the folder exists
+if [ ! -d "$outputFolder2" ]; then
+  # If it doesn't exist, create it and its parent directories recursively
+  mkdir -p "$outputFolder2"
+  echo "Folder '$outputFolder2' created."
+else
+  echo "Folder '$outputFolder2' already exists."
+fi
 
 # Remember the list of .js files in the current directory and their paths
 files=$(find "$(pwd)/tests" -name '*.js' -type f)
-
-# Get the path of the directory where the script is located
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Change to the desired directory
 cd ../../../
@@ -16,15 +25,15 @@ cd ../../../
 for file in $files
 do
   # Get the path of the current file relative to the original directory
-  relative_path=${file#$(pwd)/}
+  relativePath=${file#$(pwd)/}
 
   # Construct the docker command
   command="docker-compose run --rm -i \
           -v $(pwd):/scripts \
           -u $(id -u):$(id -g) \
-          k6 run $relative_path \
+          k6 run $relativePath \
           --summary-trend-stats='avg,min,med,max,p(90),p(95),count' \
-          --out json=/scripts/results/result_$timestamp.json"
+          --out json='/scripts/$outputFolder/$reportFile'"
 
   echo "Running command: $command"
 
