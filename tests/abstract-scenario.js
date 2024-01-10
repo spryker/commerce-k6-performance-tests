6b4ae7a713093c313198d814d1487c391607331c
Florian Kraemer <florian.kraemer@spryker.com>
2024-01-10 01:32:23 +0100

    Fixing a bug in abstract-scenario.js

--- tests/abstract-scenario.js
+++ tests/abstract-scenario.js
@@ -57,7 +57,7 @@ export class AbstractScenario {
 
     assertResponseBodyIncludes(response, text) {
         return check(response, {
-            [`Verify ${text} text`]: (r) => r.body.includes(text),
+            [`Verify ${text} text`]: (r) => r.body && r.body.includes(text),
         })
     }
 
