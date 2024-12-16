a6534b2c8b6743a274eb53f86f6cc47fd00cb9e1
ilyakubanov <ilya.kubanov@spryker.com>
2024-12-16 12:04:45 +0100

    Improvements

---
+++ shell/run-all-suite-performance-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source shell/functions.sh
+
+# Record the start time
+start_timer
+
+# Create arrays for file lists
+filesDirectory1=($(find "tests/suite/sapi/tests" -name '*.js' -type f))
+
+# Merge the arrays
+files=("${filesDirectory1[@]}")
+
+run_k6_tests "$files"
+
+# Record the end time
+stop_timer
